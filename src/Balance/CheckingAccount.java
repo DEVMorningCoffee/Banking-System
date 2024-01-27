cd5d11a107a8fad5dc8684622805d656e31b1720
DEVMorningCoffee <jeffreyabraham.sep+github@gmail.com>
2024-01-26 20:12:56 -0500

    Both class inheritance the BasicAccount class

--- src/Balance/CheckingAccount.java
+++ src/Balance/CheckingAccount.java
@@ -1,26 +1,7 @@
-package User;
+package Balance;
+import Balance.BasicAccount;
 
 import java.text.DecimalFormat;
 
-public class CheckingAccount {
-    private float balance = 1100.99f;
-
-    private void setBalance(float setAmount){
-        this.balance = formatBalance(setAmount);
-    }
-
-    private static float formatBalance(float amount){
-        DecimalFormat decimalFormat = new DecimalFormat("0.00");
-        String formatAmount =  decimalFormat.format(amount);
-        return Float.parseFloat(formatAmount);
-    }
-
-    public float getBalance(){
-        return this.balance;
-    }
-
-    public void depositBalance(float amount){
-        amount = formatBalance(amount);
-        this.balance += amount;
-    }
+public class CheckingAccount extends BasicAccount{
 }
--- src/Balance/SavingAccount.java
+++ src/Balance/SavingAccount.java
@@ -1,26 +1,7 @@
-package User;
+package Balance;
+import Balance.BasicAccount;
 
 import java.text.DecimalFormat;
 
-public class SavingAccount {
-    private float balance = 0f;
-
-    private void setBalance(float setAmount){
-        this.balance = formatBalance(setAmount);
-    }
-
-    private static float formatBalance(float amount){
-        DecimalFormat decimalFormat = new DecimalFormat("0.00");
-        String formatAmount =  decimalFormat.format(amount);
-        return Float.parseFloat(formatAmount);
-    }
-
-    public float getBalance(){
-        return this.balance;
-    }
-
-    public void depositBalance(float amount){
-        amount = formatBalance(amount);
-        this.balance += amount;
-    }
+public class SavingAccount extends BasicAccount {
 }
